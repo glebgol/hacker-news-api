8510ad930e47b468d069e727fdda634179dc3c99
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-05-03 20:36:26 +0300

    Initial commit

---
+++ src/main/java/com/example/hackernewsapi/HackerNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackernewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerNewsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerNewsApiApplication.class, args);
+    }
+
+}
